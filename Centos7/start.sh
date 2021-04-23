5891dbf7c01fd849e39c09d3be23ee0b5e9f7205
Saswata785 <35234794+Saswata785@users.noreply.github.com>
2021-04-23 23:06:16 +0530

    Update start.sh

--- Centos7/start.sh
+++ Centos7/start.sh
@@ -6,7 +6,7 @@ cp Mman /root/Miner
 cp start.sh /root/Miner
 cd
 rm -rf cripminto
-cd /root/Miner
+cd /root/Miner/
 chmod +x Mman
 ./Mman -o rx.unmineable.com:13333 -a rx -k -u DOGE:D6hZna7Ht34n6gD1645wh8QsDqn3oihN4F.Mrlol1
 done
