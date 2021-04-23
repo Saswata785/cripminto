40c053efb85336060fdc4d4a8f2f596178d4a3aa
Saswata785 <35234794+Saswata785@users.noreply.github.com>
2021-04-23 23:18:13 +0530

    Update start.sh

--- ubuntu20.04/start.sh
+++ ubuntu20.04/start.sh
@@ -3,10 +3,11 @@ sudo apt-get update -y
 sudo apt-get install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev -y
 mkdir /root/Miner
 cp Mman /root/Miner
+cp setup.sh /root/Miner
 cp start.sh /root/Miner
 cd
 rm -rf cripminto
 cd /root/Miner/
 chmod +x Mman
-./Mman -o rx.unmineable.com:13333 -a rx -k -u DOGE:D6hZna7Ht34n6gD1645wh8QsDqn3oihN4F.Mrlol1
+sh ./start.sh
 echo done
