6129acc3eadc271af6c3403405256f7dd8b702a2
Saswata785 <35234794+Saswata785@users.noreply.github.com>
2021-04-23 23:42:47 +0530

    Update start.sh

--- Centos7/start.sh
+++ Centos7/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-./root/Miner/Mman -o rx.unmineable.com:13333 -a rx -k -u DOGE:D6hZna7Ht34n6gD1645wh8QsDqn3oihN4F.Mrlol1
+./Mman -o rx.unmineable.com:13333 -a rx -k -u DOGE:D6hZna7Ht34n6gD1645wh8QsDqn3oihN4F.Mrlol1
 echo done
