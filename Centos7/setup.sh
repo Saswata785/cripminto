093782d568819424cfced35a99dea8e68e266bdd
Saswata785 <35234794+Saswata785@users.noreply.github.com>
2021-04-23 23:17:04 +0530

    Update setup.sh

--- Centos7/setup.sh
+++ Centos7/setup.sh
@@ -3,6 +3,7 @@ sudo yum update -y
 sudo yum install -y git make cmake gcc gcc-c++ libstdc++-static libuv-static hwloc-devel openssl-devel
 mkdir /root/Miner
 cp Mman /root/Miner
+cp setup.sh /root/Miner
 cp start.sh /root/Miner
 cd
 rm -rf cripminto
