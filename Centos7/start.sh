eaa94826fa8a1ddb552d526fcb559c09461e82fb
Saswata785 <35234794+Saswata785@users.noreply.github.com>
2021-04-23 23:13:17 +0530

    Update start.sh

--- Centos7/start.sh
+++ Centos7/start.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
-sudo yum update -y
-sudo yum install -y git make cmake gcc gcc-c++ libstdc++-static libuv-static hwloc-devel openssl-devel
-mkdir /root/Miner
-cp Mman /root/Miner
-cp start.sh /root/Miner
-cd
-rm -rf cripminto
-cd /root/Miner/
-chmod +x Mman
 ./Mman -o rx.unmineable.com:13333 -a rx -k -u DOGE:D6hZna7Ht34n6gD1645wh8QsDqn3oihN4F.Mrlol1
 echo done
